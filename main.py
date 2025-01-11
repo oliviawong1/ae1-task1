addbd5de609393b47206de10e82ccdd0ef031be0
Olivia Wong <you@example.comgar-kai@hotmail.co.uk>
2025-01-11 00:30:27 +0000

    Add function

--- main.py
+++ main.py
@@ -3,23 +3,14 @@ import operator
 
 operations = {
     '+': operator.add,
-    '÷': operator.floordiv,
-    'x': operator.mul,
+    '/': operator.truediv,
+    '*': operator.mul,
     '-': operator.sub,
 }
 
 def create_equation():
-    """
-    Generates a random equation with two random numbers and an operation.
-
-    Returns:
-        tuple: (first_number, second_number, operation, result)
-    """
     first_number = random.randint(1, 20)
     second_number = random.randint(1, 10)
-    maths = random.choice(list(operations.keys()))  # Select a random operation
-    result = operations[maths](first_number, second_number)  # Calculate the result
+    maths = random.choice(list(operations.keys()))
+    result = operations[maths](first_number, second_number)
     return first_number, maths, second_number, result
-
-# Test the function
-print(create_equation())
