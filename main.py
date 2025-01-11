1e1ac584226896ef9ce214383b22bdfaec50c391
Olivia Wong <you@example.comgar-kai@hotmail.co.uk>
2025-01-11 20:17:23 +0000

    update code

--- main.py
+++ main.py
@@ -3,18 +3,36 @@ import operator
 
 def create_equation():
     operations = {
-        '+': operator.add,
-        '//': operator.floordiv,
-        '*': operator.mul,
-        '-': operator.sub,
+        "+": operator.add,
+        "*": operator.mul,
+        "-": operator.sub,
     }
 
-    first_number = random.randint(1,5)
-    second_number = random.randint(1,5)
+    first_number = random.randint(1, 5)
+    second_number = random.randint(1, 5)
     operation = random.choice(list(operations.keys()))
 
-    answer = operations[operation](first_number,second_number)
-    equation = f"{first_number} {operation} {second_number}"
-    return equation, answer
+    # Calculate the answer using the selected operation
+    answer = operations[operation](first_number, second_number)
 
-create_equation()
+    part = ["first_number","second_number","answer"]
+    missing_part = random.choice(list(part))
+
+    if missing_part == "first_number":
+        equation = f"x {operation} {second_number} = {answer}"
+        missing_value = first_number
+    
+    elif missing_part == "second_number":
+        equation = f"{first_number} {operation} x = {answer}"
+        missing_value = second_number
+    
+    else:
+        equation = f"{first_number} {operation} {second_number} = x"
+        missing_value = answer
+    
+    return equation, missing_value
+
+equation, missing_value = create_equation()
+print("Equation:", equation)
+print("Answer:", missing_value)
+
