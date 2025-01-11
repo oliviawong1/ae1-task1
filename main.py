687e8115ed721f16372ab5c7130a5920f63b5a12
Olivia Wong <you@example.comgar-kai@hotmail.co.uk>
2025-01-11 18:39:26 +0000

    Update code

--- main.py
+++ main.py
@@ -16,13 +16,28 @@ def create_equation():
     tuple: first_number, operation, second_number, result
     """
     first_number = random.randint(1, 10)
-    second_number = random.randint(1, 20)
+    second_number = random.randint(1, 10)
     operation = random.choice(list(operations.keys()))
     
     # Calculate the answer
     answer = operations[operation](first_number, second_number)
-    
-    # Return equation and answer
-    return f"{first_number} {operation} {second_number}", answer
+    #return f"{first_number} {operation} {second_number}", answer
 
-print(create_equation())
+    part = ["first_number", "second_number","answer"]
+    missing_part = random.choice(part)
+
+    if missing_part == "first_number":
+        equation = f"x {operation} {second_number} = {answer}"
+        missing_value = first_number
+    elif missing_part == "second_number":
+        equation = f"{first_number} {operation} x = {answer}"
+        missing_value = second_number
+    else: 
+        equation = f"{first_number} {operation} {second_number} = x"
+        missing_value = answer
+    
+    return equation, missing_value
+
+equation, missing_value = create_equation()
+print("Equation:", equation)
+print("Missing value:", missing_value)
